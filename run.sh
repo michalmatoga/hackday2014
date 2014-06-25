bcc528fb1377a745672b23470310f70bed7b5966
michmato <michal.matoga@schibsted.pl>
2014-06-25 10:02:25 +0200

    Change path to arduino-serial executable

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 # while :; do afplay /path/to/song.mp3; done
 while :
 do
-    ./arduino-serial/arduino-serial -b 9600 -p /dev/tty.usbmodem1421 -r | grep "read"
+    vendor/arduino-serial/arduino-serial -b 9600 -p /dev/tty.usbmodem1421 -r | grep "read"
 done
